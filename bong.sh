63d11e52ceb0a9c4d688072820b31bdfb49374d4
Lawrence Bogdan Aleksandrov <malwareinform@gmail.com>
2014-12-03 20:34:14 +0800

    2.0 baby
    perpendicular skeleton update

--- bong.sh
+++ bong.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-function install {
-        echo `wget https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch-dev && mv screenfetch-dev screenfetch && chmod +x screenfetch && mv screenfetch /usr/bin`
+script=$0
+script=`basename ${script}`
+
+function checkIfInstalled {
+        if [ -e /usr/bin/screenfetch ]; then
+                echo "Screenfetch already installed!"
+        else
+                echo `wget https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch-dev && mv screenfetch-dev screenfetch && chmod +x screenfetch && mv screenfetch /usr/bin`
+        fi
 }
 
-if [ -e /usr/bin/screenfetch ]; then
-        echo "Screenfetch already installed!"
-else
-        install
+function checkIfInstalledAndFinnaDeleteTheShit {
+        if [ -e /usr/bin/screenfetch ]; then
+                echo "Screenfetch already installed!"
+        else
+                echo `wget https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch-dev && mv screenfetch-dev screenfetch && chmod +x screenfetch && mv screenfetch /usr/bin && rm $0`
+        fi
+}
+
+echo "1. Install screenfetch"
+echo "2. Install screenfetch and delete install script"
+
+read opt
+
+if [ $opt -eq 1 ]; then
+        checkIfInstalled
+elif [ $opt -eq 2 ]; then
+        checkIfInstalledAndFinnaDeleteTheShit
 fi
+
